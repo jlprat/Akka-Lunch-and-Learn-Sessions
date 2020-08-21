8f2b585d1a49931a77eecd46256f07e04a2e9126
Josep Prat <josep.prat@springer.com>
2020-08-21 12:15:07 +0200

    Add Unit Test for TempratureStatistics

--- src/main/scala/io.github.jlprat.akka.lnl/intro/classic/TemperatureStatistics.scala
+++ src/main/scala/io.github.jlprat.akka.lnl/intro/classic/TemperatureStatistics.scala
@@ -1,10 +1,12 @@
 package io.github.jlprat.akka.lnl.intro.classic
 
 import akka.actor.Actor
+import io.github.jlprat.akka.lnl.intro.classic.TemperatureStatistics._
+import akka.actor.ActorLogging
 
 
 object TemperatureStatistics {
-  case class TempetureReading(value: Double)
+  case class TemperatureReading(value: Double)
 
   case object GetMaxTemperature
   case object GetMinTemperature
@@ -15,14 +17,40 @@ object TemperatureStatistics {
   * This class models an actor that receives read temperatures and generates statistics about it
   *
   */
-class TemperatureStatistics extends Actor {
+class TemperatureStatistics extends Actor with ActorLogging {
 
   var maxTemp: Double = Double.MinValue
   var minTemp: Double = Double.MaxValue
   var averageTemperature: Double = 0 
   var events: Long = 0
-  
-  override def receive: Actor.Receive = ???
 
+  /**
+    * Default receive handler. Until the first measure is not received it doesn't reply    *
+    */
+  override def receive: Actor.Receive = {
+    case TemperatureReading(value) => 
+      events = 1
+      maxTemp = Math.max(maxTemp, value)
+      minTemp = Math.min(minTemp, value)
+      averageTemperature = value
+      context.become(withData)
+    case _ =>
+      log.info("No Data")
+      throw new IllegalStateException("No Temperature Data")
+  }
+
+  /**
+    * Receive handler for when there is already data to perform statistics
+    */
+  private def withData: Actor.Receive = {
+    case TemperatureReading(value) => 
+      averageTemperature = ((averageTemperature * events) + value) / (events + 1)
+      events = events + 1
+      maxTemp = Math.max(maxTemp, value)
+      minTemp = Math.min(minTemp, value)
+    case GetMaxTemperature => sender() ! maxTemp
+    case GetMinTemperature => sender() ! minTemp
+    case GetAverageTemperature => sender() ! averageTemperature
+  }
   
 }
---
+++ src/test/scala/io.github.jlprat.akka.lnl/intro/classic/TemperatureStatisticsTest.scala
@@ -0,0 +1,78 @@
+package io.github.jlprat.akka.lnl.intro.classic
+
+import org.scalatest.matchers.should.Matchers
+import akka.testkit.TestActorRef
+import io.github.jlprat.akka.lnl.intro.classic.TemperatureStatistics.GetMaxTemperature
+import org.scalatest.flatspec.AnyFlatSpecLike
+import akka.testkit.TestKit
+import akka.actor.ActorSystem
+import akka.pattern.ask
+import org.scalatest.concurrent.ScalaFutures
+import scala.concurrent.duration._
+import akka.util.Timeout
+import org.scalatest.time.Seconds
+import org.scalatest.time.Span
+import org.scalatest.time.Millis
+import io.github.jlprat.akka.lnl.intro.classic.TemperatureStatistics.GetMinTemperature
+import io.github.jlprat.akka.lnl.intro.classic.TemperatureStatistics.GetAverageTemperature
+import io.github.jlprat.akka.lnl.intro.classic.TemperatureStatistics.TemperatureReading
+
+/**
+  * Unit Test approach to actors. Tends to result on whitebox testing
+  *
+  * This synchronous tests are mostly discouraged as might fail to work with advanced features:
+  * Due to the synchronous nature of TestActorRef it will not work with some support traits that Akka provides as they require asynchronous behaviors to function properly. Examples of traits that do not mix well with test actor refs are PersistentActor and AtLeastOnceDelivery provided by Akka Persistence.
+  */
+class TemperatureStatisticsTest extends TestKit(ActorSystem()) with AnyFlatSpecLike with Matchers with ScalaFutures {
+
+  implicit val timeout: Timeout = 300.millis
+  implicit val defaultPatience = PatienceConfig(timeout = Span(1, Seconds), interval = Span(5, Millis))
+  
+  "TemperatureStatistics" should "fail to get any statistic before getting any reading" in {
+    
+    val temperatureStatistics = TestActorRef[TemperatureStatistics]
+    val futureMaxTemp = temperatureStatistics ? GetMaxTemperature
+    futureMaxTemp.failed.futureValue.isInstanceOf[IllegalStateException]
+
+    val futureMinTemp = temperatureStatistics ? GetMinTemperature
+    futureMinTemp.failed.futureValue.isInstanceOf[IllegalStateException]
+
+    val futureAverageTemp = temperatureStatistics ? GetAverageTemperature
+    futureAverageTemp.failed.futureValue.isInstanceOf[IllegalStateException]
+  }
+
+  it should "read temperatures" in {
+    val temperatureStatistics = TestActorRef[TemperatureStatistics]
+    temperatureStatistics ! TemperatureReading(40.1)
+    
+    temperatureStatistics.underlyingActor.events shouldBe 1
+    temperatureStatistics.underlyingActor.averageTemperature shouldBe 40.1
+    temperatureStatistics.underlyingActor.minTemp shouldBe 40.1
+    temperatureStatistics.underlyingActor.maxTemp shouldBe 40.1
+    
+    temperatureStatistics ! TemperatureReading(34.9)
+
+    temperatureStatistics.underlyingActor.events shouldBe 2
+    temperatureStatistics.underlyingActor.averageTemperature shouldBe 37.5
+    temperatureStatistics.underlyingActor.maxTemp shouldBe 40.1
+    temperatureStatistics.underlyingActor.minTemp shouldBe 34.9
+    
+  }
+
+  it should "return statistics after reading temperatures" in {
+    val temperatureStatistics = TestActorRef[TemperatureStatistics]
+    temperatureStatistics ! TemperatureReading(40.1)
+    temperatureStatistics ! TemperatureReading(34.9)
+    
+    val futureMaxTemp = temperatureStatistics ? GetMaxTemperature
+    futureMaxTemp.futureValue shouldBe 40.1
+
+    val futureMinTemp = temperatureStatistics ? GetMinTemperature
+    futureMinTemp.futureValue shouldBe 34.9
+
+    val futureAverageTemp = temperatureStatistics ? GetAverageTemperature
+    futureAverageTemp.futureValue shouldBe 37.5
+    
+  }
+
+}
