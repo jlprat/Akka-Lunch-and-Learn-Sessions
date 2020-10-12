9971f3778e2f22c48d91600f428de83094256a79
Josep Prat <josep.prat@springer.com>
2020-10-12 16:00:18 +0200

    Add Example and slide for Typed Supervision

--- src/main/scala/io.github.jlprat.akka.lnl/supervision/typed/SupervisionExample.scala
+++ src/main/scala/io.github.jlprat.akka.lnl/supervision/typed/SupervisionExample.scala
@@ -1,57 +1,57 @@
 package io.github.jlprat.akka.lnl.supervision.typed
 
+import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy}
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.ActorRef
-import akka.actor.typed.Behavior
-import akka.actor.typed.SupervisorStrategy
+import scala.concurrent.duration._
 
 object SupervisionExample {
 
-  object Node {
+  sealed trait Command
+  case object Init extends Command
 
-    sealed trait Command
-    case class Save(tag: String, value: Int, replyTo: ActorRef[Key]) extends Command
-    case class Retrieve(key: Key, replyTo: ActorRef[Stored])         extends Command
+  sealed trait NodeCommand                                         extends Command
+  case class Save(tag: String, value: Int, replyTo: ActorRef[Key]) extends NodeCommand
+  case class Retrieve(key: Key, replyTo: ActorRef[Stored])         extends NodeCommand
 
-    case class Key(id: String)
-    case class Stored(product: Product)
+  case class Key(id: String)
+  case class Stored(product: Product)
 
-    case class Product(hash: String, tag: String, value: Int)
+  case class Product(hash: String, tag: String, value: Int)
 
-    def apply(storage: Map[String, Product] = Map.empty): Behavior[Command] =
-      Behaviors.receiveMessage {
-        case Save(tag, value, replyTo) =>
-          val hash = s"${tag.hashCode()}"
-          replyTo ! Key(hash)
-          apply(storage.updated(hash, Product(hash, tag, value)))
-        case Retrieve(key, _) if !storage.contains(key.id) =>
-          throw new IllegalStateException("No such key!")
-        case Retrieve(key, replyTo) =>
-          replyTo ! Stored(storage(key.id))
-          Behaviors.same
+  def supervise(behavior: Behavior[NodeCommand]): Behavior[NodeCommand] =
+    Behaviors
+      .supervise(behavior)
+      .onFailure[IllegalStateException](SupervisorStrategy.restart.withLimit(3, 1.minute))
 
-      }
-  }
+  def apply(): Behavior[Command] =
+    Behaviors.receive {
+      case (context, Init) =>
+        val child = context.spawnAnonymous(supervise(store()))
+        initialized(child)
+      case _ => throw new IllegalStateException("Not yet initialized")
+    }
 
-  object Guardian {
+  def initialized(child: ActorRef[NodeCommand]): Behavior[Command] =
+    Behaviors.receiveMessage {
+      case Init => throw new IllegalStateException("Already initialized")
+      case c @ Save(_, _, _) =>
+        child.tell(c)
+        Behaviors.same
+      case c @ Retrieve(_, _) =>
+        child.tell(c)
+        Behaviors.same
+    }
 
-    sealed trait Command
-    case object Init extends Command
-
-    def supervise(behavior: Behavior[Node.Command]): Behavior[Node.Command] =
-      Behaviors.supervise(behavior).onFailure[IllegalStateException](SupervisorStrategy.resume)
-
-    def apply(): Behavior[Command] =
-      Behaviors.receive {
-        case (context, Init) =>
-          context.spawnAnonymous(supervise(Node()))
-          initialized()
-      }
-
-    def initialized(): Behavior[Command] =
-      Behaviors.receiveMessage {
-        case _ => throw new IllegalStateException("Already initialized")
-      }
-
-  }
+  def store(storage: Map[String, Product] = Map.empty): Behavior[NodeCommand] =
+    Behaviors.receiveMessage {
+      case Save(tag, value, replyTo) =>
+        val hash = s"${tag.hashCode()}"
+        replyTo ! Key(hash)
+        store(storage.updated(hash, Product(hash, tag, value)))
+      case Retrieve(key, _) if !storage.contains(key.id) =>
+        throw new IllegalStateException("No such key!")
+      case Retrieve(key, replyTo) =>
+        replyTo ! Stored(storage(key.id))
+        Behaviors.same
+    }
 }
