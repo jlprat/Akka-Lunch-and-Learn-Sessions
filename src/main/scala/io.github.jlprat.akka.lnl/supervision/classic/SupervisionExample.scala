3aaf9c2202170a80ff47f4149b25ffaaab857a8c
Josep Prat <josep.prat@springer.com>
2020-10-13 13:30:15 +0200

    Add Akka Classic example

--- src/main/scala/io.github.jlprat.akka.lnl/supervision/classic/SupervisionExample.scala
+++ src/main/scala/io.github.jlprat.akka.lnl/supervision/classic/SupervisionExample.scala
@@ -1,9 +1,10 @@
 package io.github.jlprat.akka.lnl.supervision.classic
 
-import akka.actor.Actor
-import akka.actor.Props
-import io.github.jlprat.akka.lnl.supervision.classic.SupervisionExample.Child._
+import akka.actor.{Actor, ActorRef, AllForOneStrategy, Props, SupervisorStrategy}
+import scala.concurrent.duration._
 
+import io.github.jlprat.akka.lnl.supervision.classic.SupervisionExample.Child._
+import io.github.jlprat.akka.lnl.supervision.classic.SupervisionExample.Parent.Init
 
 object SupervisionExample {
 
@@ -15,7 +16,6 @@ object SupervisionExample {
 
     case class Key(id: String)
     case class Stored(product: Product)
-
     case class Product(hash: String, tag: String, value: Int)
 
     def props(): Props = Props(classOf[Child])
@@ -39,14 +39,30 @@ object SupervisionExample {
   }
 
   object Parent {
+    sealed trait Command
+    case object Init extends Command
 
+    def props(): Props = Props(classOf[Parent])
   }
 
   class Parent extends Actor {
 
-    override def receive: Actor.Receive = ???
+    override def receive: Actor.Receive = {
+      case Init =>
+        val child = context.actorOf(Child.props())
+        context.become(initialized(child))
+      case _ => throw new IllegalStateException("Not yet initialized")
+    }
 
+    def initialized(child: ActorRef): Actor.Receive = {
+      case Init => throw new IllegalStateException("Already initialized")
+      case msg  => child ! msg
+    }
 
+    override def supervisorStrategy: SupervisorStrategy =
+      AllForOneStrategy(3, 1.minute) {
+        case _: IllegalStateException => SupervisorStrategy.restart
+        case _                        => SupervisorStrategy.escalate
+      }
   }
-
 }
