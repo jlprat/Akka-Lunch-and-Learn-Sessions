7c24ef687994e72d66e5485e842ac2dbe1954b92
Josep Prat <josep.prat@springer.com>
2020-10-13 16:00:28 +0200

    Add restart examples with Akka Classic

--- src/main/scala/io.github.jlprat.akka.lnl/supervision/classic/Initialization.scala
+++ src/main/scala/io.github.jlprat.akka.lnl/supervision/classic/Initialization.scala
@@ -7,7 +7,6 @@ object Initialization {
   sealed trait Command
   case object Init extends Command
 
-
   object DBConnection
 
   def props(): Props = Props(classOf[Initialization])
@@ -24,12 +23,51 @@ class Initialization extends Actor {
   }
 
   override def receive: Actor.Receive = {
-    case Init => 
+    case Init =>
       // Initialization via Message
       initDB()
     case _ => ??? // normal actor work
   }
 
   private def initDB(): Unit = ()
-  
+
+}
+
+class Restart extends Actor {
+
+  override def preStart(): Unit = {
+    // We create our children, but we want to keep them on restarts
+    initChildren()
+  }
+
+  /**
+    * default implementation:
+    *
+    * def preRestart(@unused reason: Throwable, @unused message: Option[Any]): Unit = {
+    *   context.children.foreach { child =>
+    *     context.unwatch(child)
+    *     context.stop(child)
+    *   }
+    *   postStop()
+    * }
+    */
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    // We don't stop our children anymore!
+    postStop()
+  }
+
+  /**
+    * Default implementation
+    * def postRestart(@unused reason: Throwable): Unit = {
+    *   preStart()
+    * }
+    */
+  override def postRestart(reason: Throwable): Unit = {
+    // we do not call `preStart` so no new children are created
+  }
+
+  override def receive: Actor.Receive = ???
+
+  def initChildren(): Unit = ()
+
 }
