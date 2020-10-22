d1ae0c290d4e23de855a1e4275ab832825d50483
Josep Prat <josep.prat@springer.com>
2020-10-22 14:57:48 +0200

    Add test and better example for supervision

--- src/main/scala/io.github.jlprat.akka.lnl/supervision/typed/SupervisionExample.scala
+++ src/main/scala/io.github.jlprat.akka.lnl/supervision/typed/SupervisionExample.scala
@@ -3,6 +3,7 @@ package io.github.jlprat.akka.lnl.supervision.typed
 import akka.actor.typed.{ActorRef, Behavior, SupervisorStrategy}
 import akka.actor.typed.scaladsl.Behaviors
 import scala.concurrent.duration._
+import akka.actor.typed.ChildFailed
 
 object SupervisionExample {
 
@@ -26,13 +27,14 @@ object SupervisionExample {
   def apply(): Behavior[Command] =
     Behaviors.receive {
       case (context, Init) =>
-        val child = context.spawnAnonymous(supervise(store()))
+        val child = context.spawn(supervise(store()), "store")
+        context.watch(child)
         initialized(child)
       case _ => throw new IllegalStateException("Not yet initialized")
     }
 
   def initialized(child: ActorRef[NodeCommand]): Behavior[Command] =
-    Behaviors.receiveMessage {
+    Behaviors.receiveMessage[Command] {
       case Init => throw new IllegalStateException("Already initialized")
       case c @ Save(_, _, _) =>
         child.tell(c)
@@ -40,17 +42,22 @@ object SupervisionExample {
       case c @ Retrieve(_, _) =>
         child.tell(c)
         Behaviors.same
+    }.receiveSignal {
+      case (context, ChildFailed(_, _)) => 
+        context.log.error("I'm stopping")
+        Behaviors.stopped
     }
 
   def store(storage: Map[String, Product] = Map.empty): Behavior[NodeCommand] =
-    Behaviors.receiveMessage {
-      case Save(tag, value, replyTo) =>
+    Behaviors.receive {
+      case (_, Save(tag, value, replyTo)) =>
         val hash = s"${tag.hashCode()}"
         replyTo ! Key(hash)
         store(storage.updated(hash, Product(hash, tag, value)))
-      case Retrieve(key, _) if !storage.contains(key.id) =>
+      case (context, Retrieve(key, _)) if !storage.contains(key.id) =>
+        context.log.error("I might lost my state")
         throw new IllegalStateException("No such key!")
-      case Retrieve(key, replyTo) =>
+      case (_, Retrieve(key, replyTo)) =>
         replyTo ! Stored(storage(key.id))
         Behaviors.same
     }
---
+++ src/test/scala/io.github.jlprat.akka.lnl/supervision/typed/SupervisionExampleTest.scala
@@ -0,0 +1,83 @@
+package io.github.jlprat.akka.lnl.supervision.typed
+
+import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, ScalaTestWithActorTestKit}
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import io.github.jlprat.akka.lnl.supervision.typed.SupervisionExample.Init
+import akka.actor.testkit.typed.Effect.Spawned
+import io.github.jlprat.akka.lnl.supervision.typed.SupervisionExample.Save
+import io.github.jlprat.akka.lnl.supervision.typed.SupervisionExample.Key
+import io.github.jlprat.akka.lnl.supervision.typed.SupervisionExample.Retrieve
+import io.github.jlprat.akka.lnl.supervision.typed.SupervisionExample.Stored
+import io.github.jlprat.akka.lnl.supervision.typed.SupervisionExample.{Product => MyProduct}
+import akka.actor.testkit.typed.scaladsl.LoggingTestKit
+
+class SupervisionExampleTest extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers {
+
+  "SupervisionExample" should "spawn a child on initialization" in {
+    val supervisionBehavior = BehaviorTestKit(SupervisionExample())
+    supervisionBehavior.run(Init)
+    supervisionBehavior.expectEffectPF {
+      case eff @ Spawned(_, "store", _) => eff
+    }
+  }
+
+  it should "save and retrieve elements" in {
+    val supervisionExample = testKit.spawn(SupervisionExample())
+    supervisionExample.tell(Init)
+
+    val keyClient = testKit.createTestProbe[Key]()
+    supervisionExample.tell(Save("key1", 3, keyClient.ref))
+    val expectedKey = Key("key1".hashCode().toString())
+    keyClient.expectMessage(expectedKey)
+
+    val storedClient = testKit.createTestProbe[Stored]()
+    supervisionExample.tell(Retrieve(expectedKey, storedClient.ref))
+    val expectedValue = Stored(MyProduct(expectedKey.id, "key1", 3))
+    storedClient.expectMessage(expectedValue)
+  }
+
+  it should "fail when trying to retrieve a key that doesn't exist but be restarted" in {
+    val supervisionExample = testKit.spawn(SupervisionExample())
+    supervisionExample.tell(Init)
+
+    val nonExistingKey = "DOESN'T EXIST"
+    val storedClient   = testKit.createTestProbe[Stored]()
+
+    LoggingTestKit.error("I might lost my state").expect {
+      supervisionExample.tell(Retrieve(Key(nonExistingKey), storedClient.ref))
+    }
+
+    storedClient.expectNoMessage()
+
+    val keyClient = testKit.createTestProbe[Key]()
+    supervisionExample.tell(Save("key1", 3, keyClient.ref))
+    val expectedKey = Key("key1".hashCode().toString())
+    keyClient.expectMessage(expectedKey)
+  }
+
+  it should "fail for good when 3 errors happen within a second" in {
+    val supervisionExample = testKit.spawn(SupervisionExample())
+    supervisionExample.tell(Init)
+
+    val nonExistingKey = "DOESN'T EXIST"
+    val storedClient   = testKit.createTestProbe[Stored]()
+
+    LoggingTestKit.error("I might lost my state").expect {
+      supervisionExample.tell(Retrieve(Key(nonExistingKey), storedClient.ref))
+    }
+    LoggingTestKit.error("I might lost my state").expect {
+      supervisionExample.tell(Retrieve(Key(nonExistingKey), storedClient.ref))
+    }
+    LoggingTestKit.error("I might lost my state").expect {
+      supervisionExample.tell(Retrieve(Key(nonExistingKey), storedClient.ref))
+    }
+    LoggingTestKit.error("I'm stopping").expect {
+      supervisionExample.tell(Retrieve(Key(nonExistingKey), storedClient.ref))
+    }
+
+    val keyClient = testKit.createTestProbe[Key]()
+    supervisionExample.tell(Save("key1", 3, keyClient.ref))
+    keyClient.expectNoMessage()
+  }
+}
