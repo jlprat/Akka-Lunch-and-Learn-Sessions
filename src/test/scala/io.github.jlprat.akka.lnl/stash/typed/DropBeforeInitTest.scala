d8d18f634bdea31ccdd9e3c9440d19dfd963eb8c
Josep Prat <josep.prat@springer.com>
2020-11-03 15:32:58 +0100

    Format code

--- src/test/scala/io.github.jlprat.akka.lnl/stash/typed/DropBeforeInitTest.scala
+++ src/test/scala/io.github.jlprat.akka.lnl/stash/typed/DropBeforeInitTest.scala
@@ -3,24 +3,19 @@ package io.github.jlprat.akka.lnl.stash.typed
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
-import io.github.jlprat.akka.lnl.stash.typed.DropBeforeInit.Primes
-import io.github.jlprat.akka.lnl.stash.typed.DropBeforeInit.Status
-import io.github.jlprat.akka.lnl.stash.typed.DropBeforeInit.Discarded
-import io.github.jlprat.akka.lnl.stash.typed.DropBeforeInit.Done
-import io.github.jlprat.akka.lnl.stash.typed.DropBeforeInit.Processing
+import io.github.jlprat.akka.lnl.stash.typed.DropBeforeInit._
 
 class DropBeforeInitTest extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers {
-  
+
   "DropBeforeInitTest" should "drop 'work' messages sent before the initialization one" in {
-      val dropBeforeInitBehavior = testKit.spawn(DropBeforeInit())
+    val dropBeforeInitBehavior = testKit.spawn(DropBeforeInit())
 
-      val probe = testKit.createTestProbe[Status]()
-      dropBeforeInitBehavior.tell(Primes(3, probe.ref))
-      probe.expectMessage(Discarded)
+    val probe = testKit.createTestProbe[Status]()
+    dropBeforeInitBehavior.tell(Primes(3, probe.ref))
+    probe.expectMessage(Discarded)
 
-      dropBeforeInitBehavior.tell(Primes(66, probe.ref))
-      probe.expectMessage(Discarded)
-
+    dropBeforeInitBehavior.tell(Primes(66, probe.ref))
+    probe.expectMessage(Discarded)
   }
 
   it should "process all 'work' messages after the initialization one" in {
@@ -30,7 +25,6 @@ class DropBeforeInitTest extends ScalaTestWithActorTestKit with AnyFlatSpecLike 
     initializedBehavior.tell(Primes(3, probe.ref))
 
     probe.expectMessage(Processing)
-    probe.expectMessage(Done(List(2,3,5)))
-
+    probe.expectMessage(Done(List(2, 3, 5)))
   }
 }
